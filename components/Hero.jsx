8a7c2a26849ba9e37f01ca58b702754844095d6e
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-11-13 02:00:53 -0600

    className fixes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,22 +5,22 @@ import Image from 'next/image'
 export function Hero({ heading, message }) {
   const router = useRouter()
   return (
-    <div className=''>
-      <section class='pt-24 md:mt-0 md:h-screen flex flex-col justify-center text-center md:text-left md:flex-row md:justify-between md:items-center lg:px-48 md:px-12 px-4 bg-gradient-to-br from-pink-500 via-indigo-500 to-primary'>
-        <div class='md:flex-1 md:mr-10'>
-          <h1 class='font-pt-serif text-5xl font-bold mb-7 text-white'>
+    <div>
+      <section className='pt-24 md:mt-0 md:h-screen flex flex-col justify-center text-center md:text-left md:flex-row md:justify-between md:items-center lg:px-48 md:px-12 px-4 bg-gradient-to-br from-pink-500 via-indigo-500 to-primary'>
+        <div className='md:flex-1 md:mr-10'>
+          <h1 className='font-pt-serif text-5xl font-bold mb-7 text-white'>
             Empowering{' '}
-            <span class='bg-underline1 bg-left-bottom bg-no-repeat pb-2 bg-100%'>
+            <span className='bg-underline1 bg-left-bottom bg-no-repeat pb-2 bg-100%'>
               seamless connections
             </span>
           </h1>
-          <p class='font-pt-serif text-2xl font-normal mb-7 text-white italic'>
+          <p className='font-pt-serif text-2xl font-normal mb-7 text-white italic'>
             Conneqt smart business card helps you build and nurture new
             connections.
           </p>
-          <div class='font-montserrat'>
+          <div className='font-montserrat'>
             <button
-              class='bg-primary px-6 py-4 rounded-lg text-black mr-2 mb-2 hover:bg-secondary'
+              className='bg-primary px-6 py-4 rounded-lg text-black mr-2 mb-2 hover:bg-secondary'
               onClick={() => router.push('/auth')}
             >
               Get Started
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,16 +1,16 @@
 export function Main() {
   return (
-    <div class='h-screen flex'>
+    <div className='h-screen flex'>
       {/* <!-- Scroll wrapper --> */}
-      <div class='flex-1 flex overflow-hidden'>
+      <div className='flex-1 flex overflow-hidden'>
         {/* <!-- Scrollable container --> */}
-        <div class='flex-1 overflow-y-scroll'>
+        <div className='flex-1 overflow-y-scroll'>
           {/* <!-- Your content --> */}
         </div>
       </div>
 
       {/* <!-- Fixed sidebar --> */}
-      <div class='bg-primary w-1/2'>{/* <!-- Sidebar content --> */}</div>
+      <div className='bg-primary w-1/2'>{/* <!-- Sidebar content --> */}</div>
     </div>
   )
 }
