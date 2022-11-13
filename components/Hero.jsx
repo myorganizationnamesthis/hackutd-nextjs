dc0d6e4c62f4e5a75fbbf6aa600559c61f26989f
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-11-12 19:36:46 -0600

    Navbar and Stuff

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export function Hero ({heading, message}) {
   return (
-    <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
+    <div style={{ height: '600px' }} className='flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img'>
       {/* Overlay */}
-      <div className='absolute left-0 right-0 bottom-0 z-[2]' />
+      <div className='absolute top-0 left-0 right-0 bottom-0 z-[-5]' />
       <div className='p-5 text-highlight z-[2] mt-[-10rem]'>
         <h2 className='text-5xl font-bold'>{heading}</h2>
         <p className='py-5 text-xl'>{message}</p>
