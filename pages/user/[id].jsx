eea32563cdea8ee0ebd1eb748029f3000daaf79e
ephraim888sun <ephraim888sun@gmail.com>
2022-11-12 19:03:50 -0600

    animate card

--- pages/user/[id].jsx
+++ pages/user/[id].jsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import { React, useState } from 'react'
 import Link from 'next/link'
 
 const userData = {
@@ -35,48 +35,108 @@ const userData = {
       link: 'https://twitter.com/FM_Investing',
     },
   ],
+  college_name: 'Southern Methodist University',
+  degree: 'BA in Computer Science and Finance',
 }
 const Profile = () => {
   const Router = useRouter()
   const { id } = Router.query
 
+  const [flipCard, setFlipCard] = useState(false)
+
   return (
     <div className='bg-black h-screen overflow-y-auto'>
       <div className='h-full max-w-lg mx-auto bg-background justify-center'>
-        <div className='rounded-xl relative m-2 py-2'>
-          {/* Card */}
-          <div className='bg-primary rounded-xl h-64'>
-            {/* Profile Pic */}
-            <div className='bg-secondary rounded-t-xl pl-2 pt-2 h-32'>
-              <div className='rounded-full bg-green-500 w-32 h-32 absolute'></div>
+        {/* Test */}
+        <div className=' h-64 rounded-xl relative m-2 py-2 cursor-pointer perspective'>
+          <div
+            className={`relative w-full h-full preserve-3d duration-1000 ${
+              flipCard === false ? 'my-rotate-y-180' : ''
+            } `}
+          >
+            {/* Front Card === false*/}
+            {/* Back Card === true*/}
+            <button
+              className={`absolute w-full h-full ${
+                flipCard === false ? 'my-rotate-y-180' : ''
+              } backface-hidden`}
+              onClick={() => setFlipCard(!flipCard)}
+            >
+              <div className='bg-gradient-to-b from-cyan-500 to-blue-500 rounded-xl h-64'>
+                {/* Profile Pic */}
+                <div className='bg-gradient-to-t from-secondary rounded-t-xl pl-2 pt-2 h-32'>
+                  <div className='rounded-full bg-green-500 w-32 h-32 absolute'></div>
 
-              <Link
-                href='/'
-                className='absolute right-4 text-background font-bold italic hover:opacity-80 hover:scale-75'
-              >
-                CONNECTQ
-              </Link>
-            </div>
+                  <Link
+                    href='/'
+                    className='absolute right-4 text-secondary font-bold italic hover:opacity-80 hover:scale-75'
+                  >
+                    CONNECTQ
+                  </Link>
+                </div>
 
-            <div className='px-2 pb-4 mt-2'>
-              {/* Name */}
-              <div className='font-bold text-2xl'>{userData.name}</div>
+                <div className='px-2 pb-4 mt-2'>
+                  {/* Name */}
+                  <div className='font-bold text-2xl'>{userData.name}</div>
 
-              {/* Title */}
-              <div className='italic font-bold mt-1.5 line-clamp-2'>
-                {userData.title}
+                  {/* Title */}
+                  <div className='italic font-bold mt-1.5 line-clamp-2'>
+                    {userData.title}
+                  </div>
+                  {/* Location */}
+                  <div className='opacity-70 text-sm font-semibold'>
+                    {userData.location}
+                  </div>
+                </div>
               </div>
-              {/* Location */}
-              <div className='opacity-70 text-sm font-semibold'>
-                {userData.location}
+            </button>
+
+            {/* Back Card */}
+            <button
+              className={`absolute w-full h-full ${
+                flipCard === true ? '' : ''
+              } backface-hidden`}
+              onClick={() => setFlipCard(!flipCard)}
+            >
+              <div className='bg-gradient-to-t from-primary via-secondary to-violet-400  rounded-xl h-64'>
+                {/* Profile Pic */}
+                <div className='rounded-t-xl pl-2 pt-2 h-32 '>
+                  <div className='rounded-full bg-red-500 w-32 h-32 absolute right-2'></div>
+
+                  <Link
+                    href='/'
+                    className='absolute left-4 text-white font-bold italic hover:opacity-80 hover:scale-75'
+                  >
+                    CONNECTQ
+                  </Link>
+                </div>
+
+                <div className='px-2 pb-4 mt-2 '>
+                  {/* Name */}
+                  <div className='font-bold text-3xl text-white line-clamp-2 md:line-clamp-1'>
+                    University of Texas At Dallas
+                  </div>
+
+                  {/* Title */}
+                  <div className='italic font-bold mt-1.5 line-clamp-1 text-white'>
+                    Bachelor of Arts Computer Science
+                  </div>
+                  {/* Location */}
+                  <div className='opacity-70 text-sm font-semibold text-white'>
+                    Sophomore
+                  </div>
+                </div>
               </div>
-            </div>
+            </button>
           </div>
+        </div>
 
+        {/* Continue */}
+        <div className='rounded-xl relative m-2 py-2'>
           {/* About Me */}
-          <div className='bg-primary my-2 px-2 rounded-xl '>
+          <div className='bg-gradient-to-l from-secondary to-blue-500 mt-1 px-2 rounded-xl '>
             <div className='font-semibold'>About Me</div>
-            <div className='text-sm italic line-clamp-8 md:line-clamp-6'>
+            <div className='pl-4 text-sm italic line-clamp-8 md:line-clamp-6 text-gray-300'>
               {userData.description}
             </div>
           </div>
@@ -84,8 +144,8 @@ const Profile = () => {
           {/* Links */}
           {userData.link.map((item, index) => (
             <Link href={item.link} key={index}>
-              <div className='bg-primary rounded-xl h-16 mt-2 ml-8 px-10 relative group hover:scale-[102%]'>
-                <div className='rounded-full bg-red-500 w-14 h-14 absolute top-1 -left-6'></div>
+              <div className='bg-gradient-to-r from-primary via-secondary to-purple-500 rounded-xl h-16 mt-2 ml-8 px-10 relative group hover:scale-[102%]'>
+                <div className='rounded-full bg-red-500 w-14 h-14 absolute top-1 -left-8'></div>
                 <div className='absolute right-2 top-5 group-hover:text-white'>
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
@@ -116,7 +176,7 @@ const Profile = () => {
             <div className='flex flex-row gap-x-2 w-full mx-auto flex-wrap justify-center mt-2'>
               {userData.skills.map((skill, index) => (
                 <div className='mb-2' key={index}>
-                  <div className='rounded h-6 bg-background opacity-80 text-white px-2 hover:cursor-pointer hover:scale-[105%]'>
+                  <div className='rounded h-6 bg-gradient-to-l from-background to-black opacity-80 text-white px-2 hover:cursor-pointer hover:scale-[105%]'>
                     {skill}
                   </div>
                 </div>
@@ -125,7 +185,7 @@ const Profile = () => {
           </div>
 
           {/* Mission Statement */}
-          <div className='mx-auto w-2/3 rounded-sm border-white opacity-80 border-2'>
+          <div className='mx-auto w-2/3 rounded-sm'>
             <div className='flex flex-row text-gray-200 text-center italic text-sm '>
               We are on a mission to empower seamless connection
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
+const flipCard = plugin(function ({ addUtilities }) {
+  addUtilities({
+    '.my-rotate-y-180': {
+      transform: 'rotateY(180deg)',
+    },
+    '.left-rotate-y-180': {
+      transform: 'rotateY(-180deg)',
+    },
+    '.preserve-3d': {
+      transformStyle: 'preserve-3d',
+    },
+    '.perspective': {
+      perspective: '1000px',
+    },
+    '.backface-hidden': {
+      backfaceVisibility: 'hidden',
+    },
+  })
+})
+
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
@@ -18,5 +41,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/line-clamp')],
+  plugins: [require('@tailwindcss/line-clamp'), flipCard],
 }
