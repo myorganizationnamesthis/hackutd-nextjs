9b5258e9ca373b58f77349f376ecf3e0714d0d40
myusernamesthisreal <myusernamesthis@gmail.com>
2022-11-12 23:16:39 -0600

    feat: Public business cards

--- pages/cards/[cardId].jsx
+++ pages/cards/[cardId].jsx
@@ -43,27 +43,29 @@ export default function Card() {
             handlePageLoad();
     }, [user])
 
-    return ( <>
-        {user ? <main className="ml-4">
-            <h1 className="text-6xl font-bold mt-2">Resume Data</h1>
-            <div className="text-primary text-xl my-4">Update resume info</div>
-            <div className="max-w-[50%]">
-                {typeof resume === "object" ? Object.keys(resume).map((k, index) => {
-                    if (k === "resume") return;
-                    return <div key={index} className="flex flex-col mb-3">
-                        <label className="text-xl font-bold">{`${k}: `}</label>
-                        <input className="block w-full bg-secondary border-0 rounded ml-4 mt-2 px-4 py-2 cursor-pointer" type="text" value={resume[k]} onChange={(e) => {
-                            const newAiResponse = { ...resume };
-                            newAiResponse[k] = e.target.value;
-                            setResume(newAiResponse);
-                        }} />
+    return (<>
+        {user ?
+            <main className="ml-4">
+                <h1 className="text-6xl font-bold mt-2">Resume Data</h1>
+                <button className="block bg-secondary px-4 py-2 mt-8 rounded text-highlight disabled:opacity-50" onClick={() => router.push(`/users/${user.id}/resumes/${cardId}`)} disabled={processing}>View Published Card</button>
+                <div className="text-primary text-xl my-4">Update resume info</div>
+                <div className="max-w-[50%]">
+                    {typeof resume === "object" ? Object.keys(resume).map((k, index) => {
+                        if (k === "resume") return;
+                        return <div key={index} className="flex flex-col mb-3">
+                            <label className="text-xl font-bold">{`${k}: `}</label>
+                            <input className="block w-full bg-secondary border-0 rounded ml-4 mt-2 px-4 py-2 cursor-pointer" type="text" value={resume[k]} onChange={(e) => {
+                                const newAiResponse = { ...resume };
+                                newAiResponse[k] = e.target.value;
+                                setResume(newAiResponse);
+                            }} />
+                        </div>
+                    }) : null}
+                    <div className="flex">
+                        <button className="block bg-secondary px-4 py-2 mx-4 mt-8 rounded text-highlight disabled:opacity-50" onClick={() => router.push("/dashboard")}>Back</button>
+                        <button className="block bg-secondary px-4 py-2 mt-8 rounded text-highlight disabled:opacity-50" onClick={handleUpdate} disabled={processing}>Update</button>
                     </div>
-                }) : null}
-                <div className="flex">
-                    <button className="block bg-secondary px-4 py-2 mx-4 mt-8 rounded text-highlight disabled:opacity-50" onClick={() => router.push("/dashboard")}>Back</button>
-                    <button className="block bg-secondary px-4 py-2 mt-8 rounded text-highlight disabled:opacity-50" onClick={handleUpdate} disabled={processing}>Update</button>
                 </div>
-            </div>
-        </main> : null}
+            </main> : null}
     </>);
 }
--- pages/users/[userId]/resumes/[resumeId].jsx
+++ pages/users/[userId]/resumes/[resumeId].jsx
@@ -1,47 +1,71 @@
 import { useRouter } from 'next/router'
-import { React, useState } from 'react'
+import { React, useEffect, useState } from 'react'
 import Link from 'next/link'
+import { doc, getDoc } from "firebase/firestore";
+import { initFirebase, db } from '../../../../firebase/clientApp'
 
-const userData = {
-  name: 'Ephraim Sun',
-  title:
-    'Amazon Future Engineer | Mustang Scholar | IT Intern @ Scout Energy Partners | CS and Finance at SMU',
-  location: 'Dallas, TX',
-  description:
-    'With a passionate interest in Computer Science, I love to design projects and solve problems, whether that be working with a team or spending my free time learning about a new concept.',
-  email: '',
-  skills: [
-    'Python',
-    'Java',
-    'Javascript',
-    'React',
-    'Next.js',
-    'HTML',
-    'CSS',
-    'Tableau',
-  ],
-  number: '4698036844',
-  link: [
-    {
-      name: 'LinkedIn',
-      link: 'https://www.linkedin.com/in/ephraim-sun/',
-    },
-    {
-      name: 'Github',
-      link: 'https://github.com/ephraim888sun',
-    },
-    {
-      name: 'Twitter',
-      link: 'https://twitter.com/FM_Investing',
-    },
-  ],
-  college_name: 'Southern Methodist University',
-  degree: 'BA in Computer Science and Finance',
-  grade: 'Sophomore',
-}
+// const userData = {
+//   name: 'Ephraim Sun',
+//   title:
+//     'Amazon Future Engineer | Mustang Scholar | IT Intern @ Scout Energy Partners | CS and Finance at SMU',
+//   location: 'Dallas, TX',
+//   description:
+//     'With a passionate interest in Computer Science, I love to design projects and solve problems, whether that be working with a team or spending my free time learning about a new concept.',
+//   email: '',
+//   skills: [
+//     'Python',
+//     'Java',
+//     'Javascript',
+//     'React',
+//     'Next.js',
+//     'HTML',
+//     'CSS',
+//     'Tableau',
+//   ],
+//   number: '4698036844',
+//   link: [
+//     {
+//       name: 'LinkedIn',
+//       link: 'https://www.linkedin.com/in/ephraim-sun/',
+//     },
+//     {
+//       name: 'Github',
+//       link: 'https://github.com/ephraim888sun',
+//     },
+//     {
+//       name: 'Twitter',
+//       link: 'https://twitter.com/FM_Investing',
+//     },
+//   ],
+//   college_name: 'Southern Methodist University',
+//   degree: 'BA in Computer Science and Finance',
+//   grade: 'Sophomore',
+// }
+
+initFirebase();
+
 const Profile = () => {
   const Router = useRouter()
-  const { id } = Router.query
+  const { userId, resumeId } = Router.query
+  const [userData, setUserData] = useState(null);
+
+  const handlePageLoad = async () => {
+    console.log("Params", Router.query); 
+    const docRef = doc(db, "users", userId, "resumes", resumeId);
+    const resDoc = await getDoc(docRef);
+    if (resDoc.exists()) {
+      console.log("Document data:", resDoc.data());
+      setUserData(resDoc.data());
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  }
+
+  useEffect(() => {
+    if (Object.keys(Router.query).length > 0) 
+      handlePageLoad();
+  }, [Router.query])
 
   const [flipCard, setFlipCard] = useState(false)
 
@@ -51,16 +75,14 @@ const Profile = () => {
         {/* Test */}
         <div className=' h-64 rounded-xl relative m-2 py-2 cursor-pointer perspective'>
           <div
-            className={`relative w-full h-full preserve-3d duration-1000 ${
-              flipCard === false ? 'my-rotate-y-180' : ''
-            } `}
+            className={`relative w-full h-full preserve-3d duration-1000 ${flipCard === false ? 'my-rotate-y-180' : ''
+              } `}
           >
             {/* Front Card === false*/}
             {/* Back Card === true*/}
             <button
-              className={`absolute w-full h-full ${
-                flipCard === false ? 'my-rotate-y-180' : ''
-              } backface-hidden`}
+              className={`absolute w-full h-full ${flipCard === false ? 'my-rotate-y-180' : ''
+                } backface-hidden`}
               onClick={() => setFlipCard(!flipCard)}
             >
               <div className='bg-gradient-to-b from-cyan-500 to-blue-500 rounded-xl h-64'>
@@ -78,15 +100,15 @@ const Profile = () => {
 
                 <div className='px-2 pb-4 mt-2'>
                   {/* Name */}
-                  <div className='font-bold text-2xl'>{userData.name}</div>
+                  <div className='font-bold text-2xl'>{userData?.name}</div>
 
                   {/* Title */}
                   <div className='italic font-bold mt-1.5 line-clamp-2'>
-                    {userData.title}
+                    {userData?.title}
                   </div>
                   {/* Location */}
                   <div className='opacity-70 text-sm font-semibold'>
-                    {userData.location}
+                    {userData?.location}
                   </div>
                 </div>
               </div>
@@ -94,9 +116,8 @@ const Profile = () => {
 
             {/* Back Card */}
             <button
-              className={`absolute w-full h-full ${
-                flipCard === true ? '' : ''
-              } backface-hidden`}
+              className={`absolute w-full h-full ${flipCard === true ? '' : ''
+                } backface-hidden`}
               onClick={() => setFlipCard(!flipCard)}
             >
               <div className='bg-gradient-to-t from-primary via-secondary to-violet-400 rounded-xl h-64'>
@@ -115,16 +136,16 @@ const Profile = () => {
                 <div className='px-2 pb-4 mt-1 '>
                   {/* Name */}
                   <div className='font-bold text-3xl text-white line-clamp-2 md:line-clamp-1'>
-                    {userData.college_name}
+                    {userData?.college_name}
                   </div>
 
                   {/* Title */}
                   <div className='italic font-bold mt-1.5 line-clamp-1 text-white'>
-                    {userData.degree}
+                    {userData?.degree}
                   </div>
                   {/* Location */}
                   <div className='opacity-70 text-sm font-semibold text-white'>
-                    {userData.grade}
+                    {userData?.grade}
                   </div>
                 </div>
               </div>
@@ -135,7 +156,7 @@ const Profile = () => {
         {/* Save Contact */}
         <div className='flex flex-row pt-2 m-2 gap-x-2 '>
           <Link
-            href={`tel:${userData.number}`}
+            href={`tel:${userData?.number}`}
             className='flex flex-row bg-gradient-to-r from-secondary to-blue-500 rounded-xl justify-center items-center flex-auto hover:scale-[101%] hover:text-white hover:cursor-pointer	 '
           >
             <div className='font-bold text-2xl'>Contact</div>
@@ -168,12 +189,12 @@ const Profile = () => {
           <div className='bg-gradient-to-l from-secondary to-blue-500 mt-1 px-2 rounded-xl '>
             <div className='font-semibold'>About Me</div>
             <div className='pl-4 text-sm italic line-clamp-5 md:line-clamp-3 text-gray-300'>
-              {userData.description}
+              {userData?.description}
             </div>
           </div>
 
           {/* Links */}
-          {userData.link.map((item, index) => (
+          {userData?.link?.map((item, index) => (
             <Link href={item.link} key={index}>
               <div className='bg-gradient-to-r from-primary via-secondary to-purple-500 rounded-xl h-16 mt-2 ml-8 px-10 relative group hover:scale-[102%]'>
                 <div className='rounded-full bg-red-500 w-14 h-14 absolute top-1 -left-8'></div>
@@ -205,7 +226,7 @@ const Profile = () => {
           <div className='bg-primary my-2 px-2 rounded-xl '>
             <div className='font-semibold'>Skills</div>
             <div className='flex flex-row gap-x-2 w-full mx-auto flex-wrap justify-center mt-2'>
-              {userData.skills.map((skill, index) => (
+              {userData?.skills.map((skill, index) => (
                 <div className='mb-2' key={index}>
                   <div className='rounded h-6 bg-gradient-to-l from-background to-black opacity-80 text-white px-2 hover:cursor-pointer hover:scale-[105%]'>
                     {skill}
