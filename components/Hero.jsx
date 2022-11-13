3ebbeab8d9e2c7594769ed8cc3283398797b5ae9
ephraim888sun <ephraim888sun@gmail.com>
2022-11-12 22:11:27 -0600

    update navbar

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import { useRouter } from "next/router"
+import React from 'react'
+import { useRouter } from 'next/router'
 
 export function Hero({ heading, message }) {
-  const router = useRouter();
+  const router = useRouter()
   return (
-    <div className='z-[-5] flex items-center justify-center h-screen bg-center bg-cover custom-img'>
-      <div className='block absolute top-[4.5rem] h-screen left-0 right-0 bottom-0 bg-black opacity-60' />
+    <div className=' flex items-center justify-center h-screen bg-center bg-cover custom-img'>
+      {/* <div className='block absolute top-[4.5rem] h-screen left-0 right-0 bottom-0 bg-black opacity-60' /> */}
       {/* Overlay */}
       <div className='p-5 text-highlight mt-[-10rem]'>
         <h2 className='text-5xl font-bold'>{heading}</h2>
         <p className='py-5 text-xl'>{message}</p>
-        <button className='z-[100] text-background bg-highlight px-8 py-2 border border-accent rounded hover:bg-primary duration-300' onClick={() => router.push("/auth")}>Get Started</button>
+        <button
+          className='z-[100] text-background bg-highlight px-8 py-2 border border-accent rounded hover:bg-primary duration-300'
+          onClick={() => router.push('/auth')}
+        >
+          Get Started
+        </button>
       </div>
     </div>
-  );
-};
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,22 @@ import { useEffect } from 'react'
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Head>
         <title>Home | ConnectApp</title>
       </Head>
       <Hero heading='CONNEQT' message='Professional Connections Made Easy.' />
       <Main />
-    </>
+    </div>
   )
 }
 
-{/* </>
+{
+  /* </>
       <body>
         {user ? null : <div className='text-primary text-2xl'>
           Welcome to CONNEQT! This application lets you create a digital business card from just a resume! Please sign in to begin...
         </div>}
       </body>
-    </> */}
+    </> */
+}
