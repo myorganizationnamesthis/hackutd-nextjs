f42467e51b033d588385c5bdcc3df6f8a696505b
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-11-13 08:32:47 -0600

    feat: Animation cleanup

--- components/Longcard.jsx
+++ components/Longcard.jsx
@@ -4,27 +4,15 @@ import React from 'react'
 const Longcard = ({ description, side, logoUrl, width, height }) => {
   return (
     <div className='hover:scale-105 transition-all'>
-        {side > 0 &&
-          <div className='flex bg-highlight rounded-lg shadow-md w-[90vw]'>
-            <div className='h-48 w-[250px] flex flex-row items-center justify-center '>
-              <Image className='ml-6' src={logoUrl} alt='Product' width={width} height={height} />
+        {true &&
+          <div className={side > 0 ? 'flex flex-row bg-highlight rounded-lg shadow-md w-[90vw]' : 'flex flex-row-reverse bg-highlight rounded-lg shadow-md w-[90vw]'}>
+            <div className='h-48 w-[250px] flex items-center justify-center '>
+              <Image className={side > 0 ? 'ml-6' : 'mr-6'} src={logoUrl} alt='Product' width={width} height={height} />
             </div>
-            <div className='mt-1 md:mr-[10vw] max-w-[90%] flex-row px-5 pb-5 mx-auto'>
-              <h5 className='text-xl font-semibold tracking-tight text-gray-900 pr-[10vw] pt-8'>
+            <div className={side > 0 ? 'mt-1 md:mr-[10vw] max-w-[90%] flex-row px-5 pb-5 mx-auto' : 'mt-1 md:ml-[10vw] max-w-[90%] flex-row px-5 pb-5 mx-auto'}>
+              <h5 className={side > 0 ? 'text-xl font-semibold tracking-tight text-gray-900 pr-[10vw] pt-8' : 'text-xl font-semibold tracking-tight text-gray-900 pl-[10vw] pt-8'}>
                 {description}
               </h5>
-            </div>
-          </div>
-        }
-        {side < 0 &&
-          <div className='flex bg-highlight rounded-lg shadow-md w-[90vw]'>
-            <div className='mt-1 md:ml-[10vw] max-w-[90%] flex-row px-5 pb-5 mx-auto'>
-              <h5 className='text-xl font-semibold tracking-tight text-gray-900 pl-[10vw] pt-8'>
-                {description}
-              </h5>
-            </div>
-            <div className='h-48 w-[250px] flex flex-row items-center justify-center'>
-              <Image className='mr-6' src={logoUrl} alt='Product' width={width} height={height} />
             </div>
           </div>
         }
