7b0c8b918e2d7808a74c6f5e814ad80d1bc8eb93
ephraim888sun <ephraim888sun@gmail.com>
2022-11-13 06:08:50 -0600

    fix auth margin

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -1,45 +1,44 @@
-import React, { useState, useEffect } from "react";
-import StyledFirebaseAuth from "../components/StyledFirebaseAuth";
-import {
-    getAuth,
-    GoogleAuthProvider,
-    EmailAuthProvider
-} from "firebase/auth";
-import { initFirebase } from "../firebase/clientApp";
-import { useUser } from "../firebase/useUser";
+import React, { useState, useEffect } from 'react'
+import StyledFirebaseAuth from '../components/StyledFirebaseAuth'
+import { getAuth, GoogleAuthProvider, EmailAuthProvider } from 'firebase/auth'
+import { initFirebase } from '../firebase/clientApp'
+import { useUser } from '../firebase/useUser'
 
 const uiConfig = {
-    signInSuccessUrl: "/",
-    signInOptions: [
-        GoogleAuthProvider.PROVIDER_ID,
-        {
-            provider: EmailAuthProvider.PROVIDER_ID,
-            requireDisplayName: true,
-        }
-    ],
-    credentialHelper: "none",
-};
+  signInSuccessUrl: '/',
+  signInOptions: [
+    GoogleAuthProvider.PROVIDER_ID,
+    {
+      provider: EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: true,
+    },
+  ],
+  credentialHelper: 'none',
+}
 
-initFirebase();
+initFirebase()
 
-const auth = getAuth();
+const auth = getAuth()
 
 export default function Auth() {
-    const [viewAuth, setViewAuth] = useState(false);
-    const {user} = useUser();
+  const [viewAuth, setViewAuth] = useState(false)
+  const { user } = useUser()
 
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            setViewAuth(true);
-        }
-        if (user) {
-            window.location.href = "/dashboard";
-        }
-    }, [user]);
-    return (
-        <div>
-            {viewAuth ?
-                <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} /> : null}
-        </div>
-    );
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setViewAuth(true)
+    }
+    if (user) {
+      window.location.href = '/dashboard'
+    }
+  }, [user])
+  return (
+    <div className='overflow-y-auto bg-background h-screen'>
+      <div className='mt-24 '>
+        {viewAuth ? (
+          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+        ) : null}
+      </div>
+    </div>
+  )
 }
